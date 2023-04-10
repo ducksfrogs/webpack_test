f750cfac06def1ad0cc234a284050d2eac2157e2
ducksfrogs <ma_yamaki@yahoo.com>
2023-04-11 06:26:56 +0900

    day1 changed

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
     module:{
         rules: [
             {
